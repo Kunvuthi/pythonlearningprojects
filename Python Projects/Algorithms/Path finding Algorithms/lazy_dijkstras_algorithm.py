b3f8919275bb7cf8a9e9615fd605491ccac56479
Kunvuthi <43140224+Kunvuthi@users.noreply.github.com>
2020-07-27 17:36:29 +0700

    Update with finding shortest path algorithm

--- Python Projects/Algorithms/Path finding Algorithms/lazy_dijkstras_algorithm.py
+++ Python Projects/Algorithms/Path finding Algorithms/lazy_dijkstras_algorithm.py
@@ -18,8 +18,9 @@ class Graph:
         self.num_nodes = len(self.data)
 
 
-    def dijkstra(self, start):
+    def dijkstra(self, start): ## if want to find shortest distance with better performance, look at the bottom's comment!
         vis = [False] * self.num_nodes
+        prev = [None] * self.num_nodes
         dist = [inf] * self.num_nodes
         dist[0] = 0
 
@@ -29,20 +30,41 @@ class Graph:
         while not pq.empty():
             index, MinDist = pq.get()
             vis[index] = True
+
+            ## Optimising to ignore distance that is smaller than the current distance in dist array
             if dist[index] < MinDist:
                 continue
             for edge in self.data[index]:
-                ## if node is already visited to ##
+                ## if node is already visited then continue ##
                 if vis[edge[0]]:
                     continue
 
                 new_dist = dist[index] + edge[1]
 
                 if new_dist < dist[edge[0]]:
+                    prev[edge[0]] = index
                     dist[edge[0]] = new_dist
                     pq.put((edge[0], new_dist))
-        return dist
+            # if index == end_:
+            #     return dist[end]           
+        return (dist, prev)
 
+    def shortest_path_using_dijk(self, start, end):
+        dist, prev = self.dijkstra(start)
+        path = []
+        at = end
+        if dist[end] == inf:
+            return path
+        while prev[at]:
+            at = prev[at]
+            path.append(at)
+        path.reverse()
+        return path
+        
+## Keep in mind that this implementation will find ALL the shortest distance from the start node first before finding the shortest path from start node to end node ##
+## If you only wanted to find the distance for neccessary path to get to the end node which may increase the speed of this program, you can put a condition outside of the for loop ##
+## checking if index == end, then stop and return dist[end].   ## 
+## Also don't forget to pass a parameter change and argument of 'end' in dijkstra() method ##
 
 arr = [[(1,7)],
        [(2,5),(3,2),(5,10)],
@@ -52,7 +74,9 @@ arr = [[(1,7)],
        [(7,2), (8,4)],
        [(5,5)],
        [(8,5)],
+       [(9,1)],
        [(8,4)]]
     
 g = Graph(arr)
+print(g.shortest_path_using_dijk(0,9))
 print(g.dijkstra(0))
